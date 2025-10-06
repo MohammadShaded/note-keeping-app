4af9a7528c978a58a485ff56eb73e7423b22a825
MohammadShadid <mohammadshadid03@gmail.com>
2025-10-06 08:06:40 +0300

    add note Mongoose model with validation

---
+++ src/models/Note.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const noteSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+    maxlength: 100
+  },
+  content: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Note = mongoose.model('Note', noteSchema);
+export default Note;
